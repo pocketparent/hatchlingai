194650ba4c4bdbb3f517dcd31dcfb7e09eb619e2
pocketparent <dianamezzanotte@gmail.com>
2025-04-15 20:47:34 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,13 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure Vite knows where to look
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
